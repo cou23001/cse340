a41ee86f3024cea61df60bc5f6f7a872a4e5e65d
cou23001 <cou23001@byui.edu>
2023-11-07 21:26:48 -0700

    assignment 2

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -243,3 +243,18 @@ VALUES   (
     'White',
     5
   );
+
+-- 4) Modify the "GM Hummer" record to read "a huge interior" rather than "small interiors"
+UPDATE public.inventory
+SET inv_description = REPLACE (inv_description,'the small interiors','a huge interior')
+WHERE inv_id = 10;
+
+-- 6) Update all records in the inventory table to add "/vehicles" to the 
+-- middle of the file path in the inv_image and inv_thumbnail columns
+UPDATE public.inventory
+SET inv_description = REPLACE (inv_description,'/images/','/images/vehicles/')
+;
+
+UPDATE public.inventory
+SET inv_thumbnail = REPLACE (inv_thumbnail,'/images/','/images/vehicles/')
+;
